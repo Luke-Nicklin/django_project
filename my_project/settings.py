2ed3cdfdad57bc186c317077b3fa664571aebf7d
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-04-16 15:02:36 +0100

    Update code for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0y+1^(p5t2vun1uz^sa72*#%8%do2(-0*dty&7*xf2!1wd9)21'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
